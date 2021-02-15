42a152eac7f68d734e9a65eae4a7a02dd552a212
Luis Ferrer <ferrerluis97@gmail.com>
2021-02-15 01:32:12 -0500

    fontsource for font management

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
+import '@fontsource/playfair-display/700.css'
 
 const Hi = styled.div`
   text-align: center;
+  font-family: "Playfair Display";
+  font-size: 2em;
+  font-weight: 700;
 `
 
 export default function Home() {
-  return <Hi>Hello world!</Hi>
+  return <Hi>Let’s Learn and Thrive. Together.</Hi>
 }
