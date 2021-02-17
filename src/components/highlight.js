4b5c466ff3969fd69d5de19da3845f6cbf698bdb
Luis Ferrer <ferrerluis97@gmail.com>
2021-02-17 01:14:43 -0500

    Making highlights highlight fully on hover

--- src/components/highlight.js
+++ src/components/highlight.js
@@ -1,18 +1,15 @@
 import styled from 'styled-components'
 
 const Highlight = styled.span`
-  transition: background 1s;
+  transition: background 0.1s;
   background: -webkit-linear-gradient(rgba(255,255,0,0) 50%, ${props => props.color} 50%);
   background: -o-linear-gradient(rgba(255,255,0,0) 50%, ${props => props.color} 50%);
   background: -moz-linear-gradient(rgba(255,255,0,0) 50%, ${props => props.color} 50%);
   background: linear-gradient(rgba(255,255,0,0) 50%, ${props => props.color} 50%);
 
   &:hover {
-    transition: background 1s;
-    background: -webkit-linear-gradient(rgba(255,255,0,0) 10%, ${props => props.color} 50%, rgba(255,255,0,0) 40%);
-    background: -o-linear-gradient(rgba(255,255,0,0) 10%, ${props => props.color} 50%, rgba(255,255,0,0) 40%);
-    background: -moz-linear-gradient(rgba(255,255,0,0) 10%, ${props => props.color} 50%, rgba(255,255,0,0) 40%);
-    background: linear-gradient(rgba(255,255,0,0) 10%, ${props => props.color} 50%, rgba(255,255,0,0) 40%);
+    transition: background 0.1s;
+    background-color: ${props => props.color};
   }
 `
 
